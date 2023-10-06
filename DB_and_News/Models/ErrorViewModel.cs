836856238bc3888a67057c39032da9c0dc4adb98
SergeiySt <ser.st.1989@gmail.com>
2023-10-06 22:48:59 +0300

    add_1

---
+++ DB_and_News/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DB_and_News.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
